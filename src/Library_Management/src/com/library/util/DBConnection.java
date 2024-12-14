be8ae983be66c9ac706e8c2a43c1f86db5bbcbc2
Sinchana K R <121605840+Sinchana12@users.noreply.github.com>
2024-12-14 12:55:39 +0530

    Update DBConnection.java

--- src/Library_Management/src/com/library/util/DBConnection.java
+++ src/Library_Management/src/com/library/util/DBConnection.java
@@ -7,12 +7,27 @@ public class DBConnection {
 	private static final String url="jdbc:mysql://localhost:3306/LibraryDB";
 	private static final String username="root";
 	private static final String password="MySQL";
+	private static Connection connection;
+
 	public static Connection getConnection() {
 		try {
-			return DriverManager.getConnection(url,username,password);
+			if(connection== null || connection.isClosed() ) {
+				connection= DriverManager.getConnection(url,username,password);
+			}
 		}catch(Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException("Error");
+		}
+		return connection;
+	}
+	
+	public static void closeConnection() {
+		try {
+			if(connection != null && !connection.isClosed()) {
+				connection.close();
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
